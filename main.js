b7d2a2697d530084acd188017d304f3760f9932a
Christopher McNeal <camcneal@crimson.ua.edu>
2016-10-16 23:32:05 -0500

    meh

--- main.js
+++ main.js
@@ -3,25 +3,69 @@ var header = $('<header>Hello</header>');
 
 var imgArray = [{
     url: "images/basketball.jpeg",
-    name: 'sports'
+    name: 'Sports'
 }, {
     url: "images/beach.jpeg",
-    name: 'vacations'
+    name: 'Vacations'
 }, {
     url: "images/benz.jpeg",
-    name: 'cars'
+    name: 'Cars'
 }, {
     url: "images/nike.jpeg",
-    name: 'shoes'
+    name: 'Shoes'
 }, {
     url: "images/LA.jpeg",
-    name: 'cities'
+    name: 'Cities'
 }, {
     url: "images/dw.jpeg",
-    name: 'watches'
+    name: 'Watches'
 }];
 
 var imgVacations = [{
+    url: "images/beach.jpeg",
+    name: 'Beach'
+}, {
+    url: "images/lake.jpeg",
+    name: 'Lake'
+}, {
+    url: "images/river.jpeg",
+    name: "River"
+}];
+
+var imgSports = [{
+  url: "images/basketball.jpeg",
+  name: 'Basketball'
+}, {
+  url:"images/football.jpeg",
+  name: "Football"
+}, {
+  url: "images/soccer.jpeg",
+  name: 'Soccer'
+}];
+
+var imgCars = [{
+  url: "images/benz.jpeg",
+  name: 'Benz'
+}, {
+  url: "images/bmw.jpeg",
+  name: 'Bmw'
+}, {
+  url: "images/mustang.jpeg",
+  name: 'Mustang'
+}];
+
+var imgShoes = [{
+  url: "images/converse.jpeg",
+  name: 'Converse'
+}, {
+  url: "images/nike.jpeg",
+  name: 'Nike'
+}, {
+  url: "images/nike1.jpeg",
+  name: 'Nike1'
+}];
+
+var imgCities = [{
     url: "images/LA.jpeg",
     name: 'LA'
 }, {
@@ -32,10 +76,36 @@ var imgVacations = [{
     name: "NY"
 }];
 
+var imgWatches = [{
+  url: "images/cartier.jpeg",
+  name: 'Cartier'
+},{
+  url: "images/dw.jpeg",
+  name: 'DW'
+},{
+  url: "images/apple.jpeg",
+  name: 'Apple'
+}];
+
 console.log(imgArray[2].name);
+
+function singleImageDiv(name,url) {
+  $('.container').empty();
+  var backBtn = $('<button>back</button>');
+  $('.container').append(backBtn);
+  backBtn.on('click',function(e){
+    location.hash = location.hash.split('/')[0];
+  });
+  var imageDiv = $('<div class="single-image"></div>');
+  $('.container').append(imageDiv);
+  imageDiv.text(name);
+  imageDiv.append('<img src=' + url + '>');
+
+}
 
 //forEach fucntion cycling through item in the image array
 function makeImageDivs(arr) {
+  $('.container').empty();
     arr.forEach(function(image, i, arr) {
         console.log(image, i);
         var imageDiv = $('<div class="album"></div>');
@@ -45,30 +115,102 @@ function makeImageDivs(arr) {
         //go inside image div find the image and add click event listener
         imageDiv.find('img').on('click', function(e) {
             console.log(e.target);
-            location.hash = '#' + image.name;
             console.log(location);
+if (location.hash === ''){
+  location.hash = '#' + image.name;
+} else {
+  location.hash = location.hash + '/' + (i+1);
 
+
+}
         });
     });
 }
 
+function makeAlbumDivs(arr) {
+  $('.container').empty();
+  var sideBar = $('<aside class="sidebar"><ul></ul></aside>');
+  var listItems = imgArray.map(function(image,i,arr){
+    return ('<li><a href=#' + image.name + '>' + image.name + '</a></li>' );
+  });
+  $('.container').append(sideBar);
+  sideBar.find('ul').append(listItems);
+    arr.forEach(function(image, i, arr) {
+        console.log(image, i);
+        var imageDiv = $('<div class="album"></div>');
+        $('.container').append(imageDiv);
+        imageDiv.text(image.name);
+        imageDiv.append('<img src=' + image.url + '>');
+        //go inside image div find the image and add click event listener
+        imageDiv.find('img').on('click', function(e) {
+            console.log(e.target);
+            console.log(location);
+if (location.hash === ''){
+  location.hash = '#' + image.name;
+} else {
+  location.hash = location.hash + '/' + (i+1);
+
+
+}
+        });
+    });
+}
 
 if (location.hash === ''){
   makeImageDivs(imgArray);
 }
 
+if (location.hash === ''){
+  makeImageDivs(imgArray);
+}
+
+function checkHash() {
+  // If we're on an album page:
+  if (location.hash === '#vacations'){
+  console.log(location.hash.split('/'));
+  makeAlbumDivs(imgVacations);
+  } else if (location.hash === '#sports'){
+  makeAlbumDivs(imgSports);
+  }else if (location.hash === '#cars') {
+  makeAlbumDivs(imgCars);
+  }else if (location.hash === '#shoes') {
+  makeAlbumDivs(imgShoes);
+  }else if (location.hash === '#cities') {
+  makeAlbumDivs(imgCities);
+  }else if (location.hash === '#watches') {
+  makeAlbumDivs(imgWatches);
+  }
+  // if we're viewing an individual image:
+  else if (location.hash.split('/')[0] === '#vacations'){
+  var imageObject = (imgVacations[Number(location.hash.split('/')[1])-1]);
+  singleImageDiv(imageObject.name,imageObject.url);
+  } else if  (location.hash.split('/')[0] === '#sports'){
+   var imageObject = (imgSports[Number(location.hash.split('/')[1])-1]);
+  singleImageDiv(imageObject.name,imageObject.url);
+  }else if (location.hash.split('/')[0] === '#cars') {
+   var imageObject = (imgCars[Number(location.hash.split('/')[1])-1]);
+  singleImageDiv(imageObject.name,imageObject.url);
+  }else if (location.hash.split('/')[0] === '#shoes') {
+   var imageObject = (imgShoes[Number(location.hash.split('/')[1])-1]);
+  singleImageDiv(imageObject.name,imageObject.url);
+  }else if (location.hash.split('/')[0] === '#cities') {
+   var imageObject = (imgCities[Number(location.hash.split('/')[1])-1]);
+  singleImageDiv(imageObject.name,imageObject.url);
+  }else if (location.hash.split('/')[0] === '#watches') {
+   var imageObject = (imgWatches[Number(location.hash.split('/')[1])-1]);
+  singleImageDiv(imageObject.name,imageObject.url);
+  }
+}
+
+checkHash();
+
 $(window).on('hashchange', function(e) {
     console.log(location.hash);
-    $('.container').empty();
-if (location.hash === '#vacations'){
-  makeImageDivs(imgVacations);
-} else if (location.hash === '#sports'){
-
-} {
-
-}
+    checkHash();
 });
 console.log($);
+
+
 
 $('.container').prepend(header);
 
